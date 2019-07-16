6efa65ed723f4724969f711672b18fc01bb583bf
Derrick Ward <dward330@gmail.com>
2019-07-15 21:14:56 -0400

    Initial Commit

---
+++ DKW_HW1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DKW_HW1
+//
+//  Created by Derrick Ward on 7/15/19.
+//  Copyright © 2019 Derrick. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
